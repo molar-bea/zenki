fa580683c350a48283b9536a59e269a9ef843099
molar-bea <molarbea123@gmail.com>
2026-03-04 10:17:45 +0800

    [components] Add term component

---
+++ components/Terms.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+if(typeof(window as any).global === "undefined") {
+    (window as any).global = window;
+}
+
+export default {
+    components :{},
+};
+</script>
+
+<template>
+    <client-only>
+        <div id="textMessage" class="text-message">Terms and Condition</div>
+    </client-only>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
+<style></style>

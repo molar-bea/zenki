31bde68cdbb79d4e5756b9b004e5eab900634771
molar-bea <molarbea123@gmail.com>
2026-03-04 10:09:39 +0800

    [components] Add copyright component

---
+++ components/Copyright.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+    const year = new Date().getFullYear();
+</script>
+
+<template>
+    <client-only>
+        <footer>
+            <nav>
+                <a href="terms">Terms and Conditions</a>
+                <span>&nbsp;|&nbsp;</span>
+                <a href="policy">Privacy Policy</a>
+                <span>&nbsp;|&nbsp;</span>
+                <a href="support">Support</a>
+            </nav>
+            <p>
+                <span>&copy;&nbsp;</span>
+                <span>{{ year }}</span>
+                <span>&nbsp;Company Name. All rights reserved.</span>
+            </p>
+        </footer>
+    </client-only>
+</template>
+
+<style scoped></style>

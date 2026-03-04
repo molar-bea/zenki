388d9477c5cb2f91e261f7f95f4e43a6a55cccbc
molar-bea <molarbea123@gmail.com>
2026-03-04 10:06:02 +0800

    [pages] Add initial Vue application route

---
+++ pages/policy.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+
+import { useSeoMeta, useHead } from '@vueuse/head';
+
+const title = "Zenki | Home";
+const description = "An application to systematize admission process.";
+
+useSeoMeta({
+ title: () => title,
+ description: () => description,
+ charset: "utf-8",
+ viewport: "width=device-width, initial-scale=1.0"
+});
+
+useHead({
+ link: [
+ {rel: 'icon', type: 'image/png', href: '/logo.png'},
+ {rel: 'stylesheet', href: '/reset.css'}
+ ]
+});
+</script>
+
+<template>
+    <client-only>
+        <NavBar />
+        <Policy />
+        <Copyright />
+    </client-only>
+</template>

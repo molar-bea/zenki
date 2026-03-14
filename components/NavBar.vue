e673674fbc52c4296018250926e8b166ba910f30
molar-bea <molarbea123@gmail.com>
2026-03-14 09:22:17 +0800

    [UI] Add NavBar component with design and usage

---
+++ components/NavBar.vue
@@ -0,0 +1,20 @@
+<template>
+    <client-only>
+        <header class="zk-navbar">
+            <nav class="zk-nav-left">
+                <a href="/" class="zk-nav-home">
+                <img src="/logo.png" width=20 height=20 class="zk-nav-logo">
+                <span>Zenki</span>
+                </a>
+            </nav>
+
+            <nav class="zk-nav-right">
+                <ul>
+                    <li><a href="#" class="zk-nav-link">Checklist</a></li>
+                    <li><a href="#" class="zk-nav-link">Appointment</a></li>
+                    <li><a href="#" class="zk-nav-link">Guide</a></li>
+                </ul>
+            </nav>
+        </header>
+    </client-only>
+</template>
